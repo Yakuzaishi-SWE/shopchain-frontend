4f8886f7f940697b4df00b4ab4ef89a13d72ca84
LuBu-0 <luca_busacca@hotmail.com>
2022-02-14 15:07:41 +0100

    fix css

--- src/views/TransactionDetails.tsx
+++ src/views/TransactionDetails.tsx
@@ -1,29 +1,19 @@
 import React from "react";
 
 const TransactionDetailsView = ({ order, id, onUnlock }: { order: IOrder, id: string, onUnlock: () => void }) => {
-    return <section>
-        <div>
-            <span>Transaction ID:</span>
-            <span>{id}</span>
-        </div>
-        <div>
-            <span>Order Owner:</span>
-            <span>{order.ownerAddress}</span>
-        </div>
-        <div>
-            <span>Payed To:</span>
-            <span>{order.sellerAddress}</span>
-        </div>
-        <div>
-            <span>Amount:</span>
-            <span>{Math.floor(order.amount*100/1e18)/100 }</span>
-        </div>
-        <div>
-            <span>State:</span>
-            <span>{order.state}</span>
-        </div>
-        <button onClick={onUnlock}>unlock</button>
-    </section>;
+    return <><section>
+        <ul>
+            <li><div className="section-head">Transaction ID:</div>{id}</li>
+        <li><div className="section-head">Order Owner:</div>{order.ownerAddress}</li>
+        <li><div className="section-head">Payed To:</div>{order.sellerAddress}</li>
+        <li><div className="section-head">Amount:{Math.floor(order.amount*100/1e18)/100 }</div></li>
+            <li><div className="section-head">State:{order.state}</div></li>
+        </ul>
+    </section>
+    <div className="box-button center">
+        <button id="unlock" onClick={onUnlock}>Unlock</button>
+    </div>
+    </>;
 }
 
 export default TransactionDetailsView;
