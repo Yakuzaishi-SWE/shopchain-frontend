8d50762d6f94e659d8fd6295f7c63d39795f4530
LuBu-0 <luca_busacca@hotmail.com>
2022-02-14 18:04:39 +0100

    Another fix

--- src/views/TransactionDetails.tsx
+++ src/views/TransactionDetails.tsx
@@ -6,8 +6,8 @@ const TransactionDetailsView = ({ order, id, onUnlock }: { order: IOrder, id: st
             <li><div className="section-head">Transaction ID:</div>{id}</li>
         <li><div className="section-head">Order Owner:</div>{order.ownerAddress}</li>
         <li><div className="section-head">Payed To:</div>{order.sellerAddress}</li>
-        <li><div className="section-head">Amount:{Math.floor(order.amount*100/1e18)/100 }</div></li>
-            <li><div className="section-head">State:{order.state}</div></li>
+        <li><div className="section-head">Amount:</div>{Math.floor(order.amount*100/1e18)/100 }</li>
+            <li><div className="section-head">State:</div>{order.state}</li>
         </ul>
     </section>
     <div className="box-button center">
