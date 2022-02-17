3e93853e7ae3fd726de0efa8560b4bb89c20e760
LuBu-0 <luca_busacca@hotmail.com>
2022-02-17 16:28:10 +0100

    refactor & fix home button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return <>
         <div className = "center">
-            <Link to="/e-commerce/">Simula Pagamento</Link>
-            <Link to="/transaction/">Elenco Transazioni</Link>
+            <Link to="/e-commerce/" className = "home-button">Simula Pagamento</Link>
+            <Link to="/transaction/" className = "home-button">Elenco Transazioni</Link>
         </div>
     </>;
 };
