1955c79988d750d8cbcd766b37048260d6f2e03f
Matteo Midena <matteo.midena@protonmail.com>
2022-02-18 11:53:34 +0100

    Fixs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import { BalanceController, OrderCountController } from "controllers";
 import { Link } from "react-router-dom";
 
 const Home = () => {
     return <>
         <div className = "center">
+            <OrderCountController/>
+            <BalanceController/>
+
+
             <Link to="/e-commerce/" className = "home-button">Checkout Page</Link>
             
             <Link to="/transaction?from=buyer" className = "home-button">Your Orders</Link>
