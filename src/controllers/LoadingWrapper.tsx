ba663662692d994ae2b805bb538aeaf5373909f0
dario-HAL2 <dario@dariofurlan.com>
2022-02-20 01:22:22 +0000

    fix loading wrap

--- src/controllers/LoadingWrapper.tsx
+++ src/controllers/LoadingWrapper.tsx
@@ -2,18 +2,9 @@ import React from "react";
 import { Loading } from "resources/svg";
 
 const WaitingCall = ({ children, loaded, error }: { children?: React.ReactChild, loaded?: boolean, error?: string }) => {
-    if (loaded !== undefined && !loaded) return <Loading />;
-    if (error) return <p>{error.toString()}</p>;
-    return <>
-        {children}
-    </>;
-};
-
-export const WaitingSend = ({ children, loaded, error }: { children?: React.ReactChild, loaded?: boolean, error?: string }) => {
-    if (loaded !== undefined && !loaded) return <>
-        <p>Check your Metamask extension. The payment process may take few seconds ...</p>
+    if (loaded !== undefined && !loaded) return <div className="sweet-loading">
         <Loading />
-    </>;
+    </div>;
     if (error) return <p>{error.toString()}</p>;
     return <>
         {children}
