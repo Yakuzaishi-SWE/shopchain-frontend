f55a3f6fff2d9f072867fc31fa3eb08cf420131f
Matteo Midena <matteo.midena@protonmail.com>
2022-02-22 00:29:08 +0100

    Fix display error with no transactionsù

--- src/controllers/TransactionList.tsx
+++ src/controllers/TransactionList.tsx
@@ -1,6 +1,6 @@
 import { useAddress } from "hooks";
 import { useOrders } from "hooks/";
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect } from "react";
 import { TransactionListView } from "views";
 import { OrderState } from "types/enums";
 import WaitingCall from "./LoadingWrapper";
@@ -9,17 +9,26 @@ const TransactionListController = ({ from, state }: { from: "seller" | "buyer", 
     const address = useAddress() || undefined;
 
     const { orders, error, loaded } = useOrders({ seller: from == "seller" ? address : undefined, buyer: from == "buyer" ? address : undefined });
-
     const filteredorders = useMemo(() => orders?.filter(el => (state !== undefined) ? el.order.state === state : true), [orders, state]);
 
-    return <WaitingCall loaded={loaded} error={error}>
-        {
-            filteredorders ?
-                <TransactionListView transactions={filteredorders} from={from}/>
-                :
-                <></>
-        }
-    </WaitingCall>;
+    // check if user has transactions
+    if(orders !== undefined && orders.length > 0){
+
+        return <WaitingCall loaded={loaded} error={error}>
+            {
+                filteredorders ?
+                    <TransactionListView transactions={filteredorders} from={from}/>
+                    :
+                    <></>
+            }
+        </WaitingCall>;
+    } else {
+        return <p className="user-msg">
+            There are no transactions made by this account. <br/>
+            You may try refresh the page or change your Metamask account.
+        </p>;
+    }
+
 };
 
 export default TransactionListController;
