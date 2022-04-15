941448a1b4dd78135c7ad21cfdc45941dfb6dab5
Matteo Midena <matteo.midena@protonmail.com>
2022-04-15 17:15:57 +0000

    Other tests

--- src/core/modules/order/domain/__test__/OrderCollection.test.ts
+++ src/core/modules/order/domain/__test__/OrderCollection.test.ts
@@ -1,3 +1,4 @@
+import exp from "constants";
 import OrderStore from "../../store/OrderStore";
 import Amount from "../Amount";
 import Order from "../Order";
@@ -6,6 +7,9 @@ import OrderState from "../OrderState";
 
 const id1 = "550e8400-e29b-41d4-a716-446655440000";
 const id2 = "770e8400-e29b-41d4-a716-446655440000";
+
+const addr1 = "0x5df80424c525af935c46b14E727564f1c4B21930";
+const addr2 = "0xFAC4df168B9f306eB8f4E6f54117B27d2B6fC1d8";
 
 const orderStore : OrderStore = {
 } as OrderStore
@@ -19,26 +23,14 @@ type OrderProps = {
 };
 
 const orderProps : OrderProps = {
-    sellerAddress: "0x0",
-    ownerAddress: "0x0",
+    sellerAddress: addr1,
+    ownerAddress: addr2,
     amount: new Amount(1),
     unlockCode: 123,
     state: new OrderState("Created"),
 }
 
-// const collectionTest : OrderCollection<any> = {
-//     addOrder: jest.fn((data: any) => {return;}),
-// } as unknown as OrderCollection<any>;
-
 describe("test OrderCollection", () => {
-
-    // NOPE -> GUARDA GIU
-    // it("test addOrder", () => {
-    //     const order = new Order(orderStore, id1, orderProps);
-    //     collectionTest.addOrder(order);
-    //     expect(collectionTest.addOrder).toBeCalledTimes(1);
-    //     expect(collectionTest.addOrder).toBeCalledWith(order);
-    // })
 
     it("test addOrder", () => {
         const order = new Order(orderStore, id1, orderProps);
@@ -48,5 +40,56 @@ describe("test OrderCollection", () => {
         ordercoll.add(order);
         expect(ordercoll.orders.size).toBe(1);
         expect(ordercoll.orderarr).toContain(order);
+    })
+
+    it("test getById", () => {
+        const order = new Order(orderStore, id1, orderProps);
+        const ordercoll = new OrderCollection();
+        ordercoll.add(order);
+
+        const result = ordercoll.getById(id1);
+        expect(result).toBe(order);
+    })
+
+    it("test getBySeller", () => {
+        const order1 = new Order(orderStore, id1, orderProps);
+        const order2 = new Order(orderStore, id2, orderProps);
+        const ordercoll = new OrderCollection();
+        ordercoll.add(order1);
+        ordercoll.add(order2);
+
+        const order_array = ordercoll.getBySeller(addr1);
+        expect(order_array.length).toBe(2);
+        expect(order_array[0]).toBeInstanceOf(Order);
+        expect(order_array[1]).toBeInstanceOf(Order);
+    })
+
+    it("test getByOwner", () => {
+        const order1 = new Order(orderStore, id1, orderProps);
+        const order2 = new Order(orderStore, id2, orderProps);
+        const ordercoll = new OrderCollection();
+        ordercoll.add(order1);
+        ordercoll.add(order2);
+
+        const order_array = ordercoll.getByOwner(addr2);
+        expect(order_array.length).toBe(2);
+        expect(order_array[0]).toBeInstanceOf(Order);
+        expect(order_array[1]).toBeInstanceOf(Order);
+    })
+
+    it("test update", () => {
+        const order1 = new Order(orderStore, id1, orderProps);
+        const order2 = new Order(orderStore, id2, orderProps);
+        const ordercoll = new OrderCollection();
+
+        ordercoll.add(order1);
+
+        order1.state = new OrderState("Paid");
+        
+        ordercoll.upd(order1);  // test TRUE branch
+        ordercoll.upd(order2);  // test FALSE branch
+
+        expect(ordercoll.getById(id1)?.state).toStrictEqual(new OrderState("Paid"));
+        expect(ordercoll.getById(id2)).toBeInstanceOf(Order);
     })
 })
--- src/core/modules/order/repo/implementations/__test__/MoneyBoxOrderRepo.test.ts
+++ src/core/modules/order/repo/implementations/__test__/MoneyBoxOrderRepo.test.ts
@@ -1,17 +1,101 @@
 import MoneyBoxOrderRepo from "../MoneyBoxOrderRepo"
 import MoneyBoxManagerContract from '../../../../../provider/contracts/MoneyBoxManagerContract';
 import Address from '../../../../../provider/domain/Address';
+import PaymentDTO from "core/modules/order/dtos/PaymentDTO";
 
+const id1 = "550e8400-e29b-41d4-a716-446655440000";
+const id2 = "770e8400-e29b-41d4-a716-446655440000";
+
+const send = jest.fn(async (data: { from: string, value: number }) => { });
+const call = jest.fn(() => {});
+
+const contract: MoneyBoxManagerContract = {
+    instance: {
+        methods: {
+            newPayment: jest.fn((orderId: string, amount: number) => {
+                return { send }
+            }),
+            getMoneyBoxPayments: jest.fn((orderId: string) => {
+                return { call }
+            }), 
+        }
+    },
+} as any;
+
+const undefinedContract: MoneyBoxManagerContract = {
+    instance: null,
+} as any;
+
+const address: Address = {
+  address: "0x0",
+} as any;
 
 describe("MoneyBoxOrderRepo", () => {
+  
+  const moneyBoxOrderRepo = new MoneyBoxOrderRepo(contract, address);
+  const moneyBoxOrderRepo_undefined = new MoneyBoxOrderRepo(undefinedContract, address);
 
-    const contract: MoneyBoxManagerContract = {} as any;
-    const address: Address = {} as any
-  
-    it("should create an instance of MoneyBoxOrderRepo", () => {
-      const moneyBoxOrderRepo = new MoneyBoxOrderRepo(contract,address);
-      expect(moneyBoxOrderRepo).toBeTruthy();
-      expect(moneyBoxOrderRepo).not.toBeFalsy();
-    });
-  
+  it("should create an instance of MoneyBoxOrderRepo", () => {
+    expect(moneyBoxOrderRepo).toBeTruthy();
+    expect(moneyBoxOrderRepo).not.toBeFalsy();
   });
+
+  describe("should do a newPayment", () => {
+    it("defined contract instance", async () => {
+      await moneyBoxOrderRepo.newPayment(id1, 1);
+      expect(send).toHaveBeenCalled();
+      expect(send).toHaveBeenCalledWith({ from: address.address, value: 1 });
+      expect(contract.instance?.methods.newPayment).toHaveBeenCalled();
+      expect(contract.instance?.methods.newPayment).toHaveBeenCalledWith(id1, 1);
+    })
+    
+    it("undefined contract instance", async () => {
+        await expect(async ()=> {
+            await moneyBoxOrderRepo_undefined.newPayment("0x0", 1)
+        })
+        .rejects
+        .toThrow("Contract not loaded");
+    })
+    
+  })
+
+  describe("should get payments", () => {
+    
+    it("defined contract instance", async () => {
+        await moneyBoxOrderRepo.newPayment(id1, 1);
+        const payments = await moneyBoxOrderRepo.getPayments(id1);
+
+        expect(contract.instance?.methods.getMoneyBoxPayments).toBeCalledTimes(1);
+        expect(contract.instance?.methods.getMoneyBoxPayments).toBeCalledWith(id1);
+    })
+
+    it("undefined contract instance", async () => {
+        await expect(async ()=> {
+            await moneyBoxOrderRepo_undefined.getPayments(id1)
+        })
+        .rejects
+        .toThrow("Contract not loaded");
+    })
+  })
+
+  describe("should get the amount to fill", () => {
+    
+    it("defined contract instance", async () => {
+        await moneyBoxOrderRepo.getAmountToFill(id1);
+
+        expect(call).toHaveBeenCalled();
+        expect(contract.instance?.methods.getAmountToFill).toBeCalledTimes(1);
+        expect(contract.instance?.methods.getAmountToFill).toBeCalledWith(id1);
+    })
+
+    it("undefined contract instance", async () => {
+        await expect(async ()=> {
+            await moneyBoxOrderRepo_undefined.getAmountToFill(id1)
+        })
+        .rejects
+        .toThrow("Contract not loaded");
+    })
+    
+  })
+  
+});
