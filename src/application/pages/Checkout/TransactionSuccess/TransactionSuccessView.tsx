270bb9b1dbeca5fe77fae8f1f9b52e61823bfa75
LuBu-0 <luca_busacca@hotmail.com>
2022-04-28 16:00:03 +0200

    TransactionSuccess added

---
+++ src/application/pages/Checkout/TransactionSuccess/TransactionSuccessView.tsx
@@ -0,0 +1,16 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+export default observer(function TransactionSuccess(){
+
+    return <>
+        <h1>Transaction Completed Successfully</h1>
+        <p className="redirect">
+            <h2>Remember to release the money when you have received your package</h2>
+            <Link to="/transaction/out/">See  here</Link>
+        </p>
+    </>;
+
+});
