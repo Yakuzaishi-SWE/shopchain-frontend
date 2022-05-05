59ba0dad16ec10ca06807f5dc50087acf7dc1af4
Matteo Midena <matteo.midena@protonmail.com>
2022-05-05 18:59:47 +0000

    New home style v1

--- src/application/pages/Home/HomeView.tsx
+++ src/application/pages/Home/HomeView.tsx
@@ -2,24 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 import OrderCountController from "./OrderCount/OrderCountController";
 import BalanceController from "./Balance/BalanceController";
+import { OrderIcon } from "resources/svg";
 
 const HomeView = () => {
-    return <>
+    return <div className="page-container">
+        <div className="home-menu">
+            <Link to="/checkout" className="home-button">
+                <OrderIcon />
+                Checkout Page
+            </Link>
+            <Link to="/transaction/out/" className="home-button">
+                <OrderIcon />
+                Your Transactions
+            </Link>
+            <Link to="/transaction/in/" className="home-button">
+                <OrderIcon />   
+                Inbound Transactions
+            </Link>
+        </div>
+
+
         <div className="home-card">
-            <div className="button-box">
-                <Link to="/checkout" className="home-button">Checkout Page</Link>
-            </div>
-            <p>In shopchain until now</p>
+            <h1>Hey FraBro, welcome in Shopchain Verse!</h1>
+            <h2>Watch some of our statistics</h2>
+            <p>Total Registered Orders in our contracts</p>
             <div className="widget-box">
                 <OrderCountController />
+                <OrderCountController />
+            </div>
+
+            <p>Total FTM</p>
+            <div className="widget-box">
+                <BalanceController />
                 <BalanceController />
             </div>
-            <div className="button-box">
-                <Link to="/transaction/out/" className="home-button">Your Transactions</Link>
-                <Link to="/transaction/in/" className="home-button">Inbound Transactions</Link>
-            </div>
+            
         </div>
-    </>;
+    </div>;
 };
 
 export default HomeView;
