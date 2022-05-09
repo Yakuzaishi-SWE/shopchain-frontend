3a511bada7667a6a38679d13d6eb4aa9b5890f32
LuBu-0 <luca_busacca@hotmail.com>
2022-05-09 22:42:26 +0200

    loading popup style

--- src/application/utils/Popup.tsx
+++ src/application/utils/Popup.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Loading } from "resources/svg";
 
 function Popup({ show, children, close }: { show: boolean, children: React.ReactNode, close?: () => void }) {
     if (!show) return <></>;
     return <div className="popup">
-        <div className="popup-inner">
-            <button className="close-btn" onClick={close}>close</button>
-            {children}
+        <div className="sweet-loading">
+            <p>Check your Metamask extension. The payment process may take few seconds...</p>
+            <Loading />
         </div>
     </div>;
 }
