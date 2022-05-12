3b1c8f48617946e44bad3748e38fdb1e351307e2
Cartu <l.carturan@gmail.com>
2022-05-12 19:31:48 +0000

    fix pagine bloccate

--- src/application/layout/LockOverlay/LockOverlayView.tsx
+++ src/application/layout/LockOverlay/LockOverlayView.tsx
@@ -2,16 +2,24 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import ILockOverlayViewModel from "./ILockOverlayModel";
 import Popup from "application/utils/Popup";
+import { useLocation } from "react-router-dom";
 
 
 export default observer(function LockOverlayView({ 
     isConnected,
 }: ILockOverlayViewModel) {
+    const location = useLocation();
 
-    return <Popup show={!isConnected}>
-        <div className="lock-overlay">
-            <h1 id="ovrl">METAMASK NOT CONNECTED</h1>
-            <p id="redp">Please connect to Metamask to continue</p>
-        </div>
-    </Popup>;
+    return <>
+        {location.pathname !== "/" && location.pathname !== "/checkout" 
+            ?
+            <Popup show={!isConnected}>
+                <div className="lock-overlay">
+                    <h1 id="ovrl">METAMASK NOT CONNECTED</h1>
+                    <p id="redp">Please connect to Metamask to continue</p>
+                </div>
+            </Popup>
+            : <></>
+        }
+    </>;
 });
